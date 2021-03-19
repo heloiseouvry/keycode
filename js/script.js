86fa529625bd0ee6f425a9102230b479a1c798e9
Heloise Ouvry <heloiseouvry@gmail.com>
2021-03-19 18:48:56 +0100

    Final grid layout

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 document.addEventListener("keydown", handleKeydown);
 
+var mainContent = document.querySelector("#main-content");
+
 var eventKey = document.querySelector(".event-key .item__content");
 var eventLocation = document.querySelector(".event-location .item__content");
 var eventWhich = document.querySelector(".event-which .item__content");
 var eventCode = document.querySelector(".event-code .item__content");
 
 function handleKeydown(event){
+    mainContent.textContent = event.keyCode;
     eventKey.textContent = event.key;
     eventLocation.textContent = event.location;
     eventWhich.textContent = event.which;
